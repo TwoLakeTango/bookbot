b88dfa4e5eccc66a262a8b851ea7e7ea7a756ea8
flylikeahokie <nttalp@gmail.com>
2025-04-16 22:49:47 -0500

    added the get_num_words function for the count words lesson. Had to back out a few changes related to the following lesson as well, but the count words lesson test still passes so I should be good to start fresh next time

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return str(file_contents)
-
+#from stats import get_num_words
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    #print(book_text)
 main()
+
+def get_num_words():
+    book_text_words = get_book_text("books/frankenstein.txt").split()
+    word_count = 0
+    for word in book_text_words:
+        word_count += 1
+    print(f"{word_count} words found in the document")
+get_num_words()
